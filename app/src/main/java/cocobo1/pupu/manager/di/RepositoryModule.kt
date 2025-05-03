27068fde26dd01336a6885a81ffb96fc89d8f46d
cocobo1 <cocobotheone@gmail.com>
2025-05-03 09:21:51 +0100

    wtf

--- app/src/main/java/cocobo1/pupu/manager/di/RepositoryModule.kt
+++
@@ -1,9 +0,0 @@
-package cocobo1.pupu.manager.di
-
-import cocobo1.pupu.manager.domain.repository.RestRepository
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
-
-val repositoryModule = module {
-    singleOf(::RestRepository)
-}
--- app/src/main/java/cocobo1/pupu/manager/installer/Installer.kt
+++
@@ -1,7 +0,0 @@
-package cocobo1.pupu.manager.installer
-
-import java.io.File
-
-interface Installer {
-    suspend fun installApks(silent: Boolean = false, vararg apks: File)
-}
