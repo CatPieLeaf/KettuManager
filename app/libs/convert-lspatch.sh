12f798e514726be799ea09c56b91cc25de1e039b
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2025-10-24 21:41:31 -0300

    chore: update lspatch and use jingmatrix fork

--- app/libs/convert-lspatch.sh
+++ app/libs/convert-lspatch.sh
@@ -1,10 +1,7 @@
 # Download LSPatch jar and convert it to an aar
 # Jars cannot be used as a dependency without breaking R8 optimization
 
-# TODO: update to latest LSPatch
-#LSPATCH_URL="https://github.com/LSPosed/LSPatch/releases/download/v0.6/jar-v0.6-398-release.jar"
-
-LSPATCH_URL="https://github.com/vendetta-mod/VendettaManager/raw/5764b16a14c42d8449722f3656b2cb42019b82a8/app/libs/lspatch.jar"
+LSPATCH_URL="https://github.com/JingMatrix/LSPatch/releases/latest/download/lspatch.jar"
 LSPATCH_FILE_NAME="lspatch"
 
 function cleanup {
@@ -23,7 +20,7 @@ touch R.txt
 cat >AndroidManifest.xml <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <manifest xmlns:android="http://schemas.android.com/apk/res/android" package="org.lsposed.lspatch">
-    <uses-sdk android:minSdkVersion="28" android:targetSdkVersion="34" />
+    <uses-sdk android:minSdkVersion="28" android:targetSdkVersion="36" />
 </manifest>
 EOF
 
